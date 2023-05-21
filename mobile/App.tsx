9f70e683795c10e90002181bcbd5f04ea4835cf8
Matheus <ymatheus.h@gmail.com>
2023-05-21 15:35:25 -0300

    build(mobile): :heavy_plus_sign: install google fonts roboto and bai jamjuree

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,32 @@
 import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native'
+import { ImageBackground } from 'react-native'
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
+import blurBg from './src/assets/bg-blur.png'
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) return null
+
   return (
-    <View className="flex-1 items-center justify-center bg-zinc-950">
-      <Text className="text-5xl font-bold text-zinc-50">Rocketseat</Text>
+    <ImageBackground
+      source={blurBg}
+      className="relative flex-1 items-center bg-gray-900"
+      imageStyle={{ position: 'absolute', left: '-100%' }}
+    >
       <StatusBar style="light" translucent />
-    </View>
+    </ImageBackground>
   )
 }
